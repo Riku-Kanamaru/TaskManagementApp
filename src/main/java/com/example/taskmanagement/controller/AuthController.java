d4e85444ce6cc9411c58c535367ca481872e87f4
matsutake <r.kanamaru@cy-an.jp>
2024-05-28 16:46:02 +0900

    ログインと登録機能の追加

---
+++ src/main/java/com/example/taskmanagement/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.taskmanagement.controller;
+
+import com.example.taskmanagement.entity.UserEntity;
+import com.example.taskmanagement.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    
+    public AuthController(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute UserEntity user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.save(user);
+        return "redirect:/login";
+    }
+}
