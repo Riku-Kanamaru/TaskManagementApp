2ed8580686d8d249e741819b65c99e84f8b00d9e
matsutake <r.kanamaru@cy-an.jp>
2024-05-30 15:23:37 +0900

    ログインできません

--- src/main/java/com/example/taskmanagement/config/SecurityConfig.java
+++ src/main/java/com/example/taskmanagement/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
             )
             .formLogin(formLogin -> formLogin
                 .loginPage("/login")
+                .defaultSuccessUrl("/top",true)
                 .permitAll()
             )
             .logout(logout -> logout
